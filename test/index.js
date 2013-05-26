e789e9aee5fa7e3f9976c2c1c7c210e838277e26
Calvin French-Owen <calvin@segment.io>
2013-05-26 14:18:09 -0700

    Updating

--- test/index.js
+++ test/index.js
@@ -3,18 +3,18 @@ var cache = require('../');
 
 var start = Date.now();
 
-cache('5771779', function (err, score) {
+cache('http://paulgraham.com/twitter.html', function (err, score) {
   console.log('Elapsed 1', Date.now() - start);
   console.log('Score:', score);
 
-  cache('5771779', function (err, score) {
+  cache('http://paulgraham.com/twitter.html', function (err, score) {
     console.log('Elapsed 3', Date.now() - start);
     console.log('Score:', score);
 
   });
 });
 
-cache('5771779', function (err, score) {
+cache('http://paulgraham.com/twitter.html', function (err, score) {
   console.log('Elapsed 2', Date.now() - start);
   console.log('Score:', score);
 
