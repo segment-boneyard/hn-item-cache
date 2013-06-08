d52ff62151c7dc428968b614fcffa9bf0af3bbe8
Calvin French-Owen <calvin@segment.io>
2013-06-08 00:11:11 -0700

    Adding debug statements, repository info

--- test/index.js
+++ test/index.js
@@ -1,20 +1,39 @@
 var cache = require('../');
 var start = Date.now();
 
-cache('http://paulgraham.com/twitter.html', function (err, item) {
+var url = 'http://www.hn-button.com/';
+
+
+cache(url, function (err, item) {
   console.log('Elapsed 1', Date.now() - start);
   console.log('ID:', item.id);
   console.log('Points:', item.points);
 
-  cache('http://paulgraham.com/twitter.html', function (err, item) {
+  cache(url, function (err, item) {
     console.log('Elapsed 3', Date.now() - start);
     console.log('ID:', item.id);
     console.log('Points:', item.points);
   });
 });
 
-cache('http://paulgraham.com/twitter.html', function (err, item) {
+cache(url, function (err, item) {
   console.log('Elapsed 2', Date.now() - start);
   console.log('ID:', item.id);
   console.log('Points:', item.points);
 });
+
+
+
+setTimeout(function () {
+
+  start = Date.now();
+
+  cache(url, function (err, item) {
+    console.log('Elapsed 4', Date.now() - start);
+    console.log('ID:', item.id);
+    console.log('Points:', item.points);
+  });
+
+
+
+}, 15000);
